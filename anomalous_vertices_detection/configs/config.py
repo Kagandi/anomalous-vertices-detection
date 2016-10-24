73a00c2a08c73e4314da3ff715ba31bf60369842
dimakagan15 <dimakagan15@gmail.com>
2016-10-24 20:02:03 +0300

    temp_path added

--- anomalous_vertices_detection/configs/config.py
+++ anomalous_vertices_detection/configs/config.py
@@ -1,10 +1,13 @@
+import os
 import platform
+import tempfile
 
 from anomalous_vertices_detection.utils.label_encoder import BinaryLabelEncoder
 
 graph_max_edge_number = 10000000
 label_encoder = BinaryLabelEncoder()
 save_progress_interval = 200000
+temp_path = os.path.join(tempfile.gettempdir(), "temp_features.csv")
 if platform.system() == 'Windows':
     pass
 else:
