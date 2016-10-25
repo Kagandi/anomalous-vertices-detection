6fc4b2a79bdc9f0fb005d4fe3ee1cf09d28ba2b6
dimakagan15 <dimakagan15@gmail.com>
2016-10-25 22:32:13 +0300

    minor fix

--- examples/simple_feature_extraction.py
+++ examples/simple_feature_extraction.py
@@ -17,4 +17,4 @@ features = FeatureController(my_graph)
 # Edge feature extraction
 features.extract_features(my_graph.edges[:1000], fast_link_features, edges_output_path)
 # Vertex feature extraction
-features.extract_features(my_graph.vertices[:1000], vetices_output_path, fast_vertex_features)
+features.extract_features(my_graph.vertices[:1000], fast_vertex_features, vetices_output_path)
