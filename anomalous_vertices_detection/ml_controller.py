c28ef968a6a2aef429a6958043ddee47dfc95195
dimakagan15 <dimakagan15@gmail.com>
2016-11-01 15:36:24 +0200

    Updated functions arguments

--- anomalous_vertices_detection/ml_controller.py
+++ anomalous_vertices_detection/ml_controller.py
@@ -1,5 +1,5 @@
-from anomalous_vertices_detection.utils.exceptions import ValueNotSet
 from anomalous_vertices_detection.utils.dataset import DataSet
+from anomalous_vertices_detection.utils.exceptions import ValueNotSet
 
 
 class MlController(object):
@@ -40,32 +40,55 @@ class MlController(object):
         """
         self._test = self._learner.convert_data_to_format(features, labels, feature_id_col_name, metadata_col_names)
 
-    def evaluate_test(self):
+    def evaluate(self, features, labels=None, feature_id_col_name=None, metadata_col_names=[]):
         """Run evaluation function on the test set.
          If there is no test set ValueNotSet
          exception will be thrown.
 
+
+        Parameters
+        ----------
+        features : string
+            path to the csv file that contains the features.
+        labels : string, (default=None)
+            The name of the label field in the csv  file.
+        feature_id_col_name : string, (default=None)
+            The name of the id field in the csv  file.
+        metadata_col_names :  list[string], (default=None)
+            List contains the name of the fields that should not be loaded.
+
+
+        Returns
+        -------
+        Frame
+            Contains the classified results.
         """
-        if self._test:
-            return self._learner.get_evaluation(self._test)
+        if features:
+            if not isinstance(features, DataSet):
+                features = self._learner.convert_data_to_format(features, labels, feature_id_col_name,
+                                                                metadata_col_names)
+            return self._learner.get_evaluation(features)
         else:
             raise ValueNotSet("Test set was not defined.")
 
     def train_classifier(self):
         """Train the classifier.
         """
-        self._learner.train_classifier(self._training)
+        self._learner = self._learner.train_classifier(self._training)
+        return self
 
-    def predict(self):
-        """Return a binary classification on the test set wit.
+    def predict(self, features, labels=None, feature_id_col_name=None, metadata_col_names=[]):
+        """Return a binary classification on the test set.
 
         Returns
         -------
-get_prediction
+            The predicted label for each line  in the test set.
         """
-        return self._learner.get_prediction(self._test)
+        if not isinstance(features, DataSet):
+            features = self._learner.convert_data_to_format(features, labels, feature_id_col_name, metadata_col_names)
+        return self._learner.get_prediction(features)
 
-    def predict_class_probabilities(self):
+    def predict_class_probabilities(self, features, labels=None, feature_id_col_name=None, metadata_col_names=[]):
         """Return the probability of every item in the test set to be the positive class.
 
         Returns
@@ -73,7 +96,9 @@ get_prediction
         Frame
             DataFrame or Sframe containing the probabilities of the test being positive class.
         """
-        return self._learner.get_prediction_probabilities(self._test)
+        if not isinstance(features, DataSet):
+            features = self._learner.convert_data_to_format(features, labels, feature_id_col_name, metadata_col_names)
+        return self._learner.get_prediction_probabilities(features)
 
     def k_fold_validation(self, k=10):
         """Return k-fold validation results.
@@ -106,13 +131,14 @@ get_prediction
         """
         return self._learner.validate_prediction_by_links(result)
 
-    def classify_by_links_probability(self, labels={"neg": 0, "pos": 1}):
+    def classify_by_links_probability(self, features, labels=None, feature_id_col_name=None, metadata_col_names=[],
+                                      labels_map={"neg": 0, "pos": 1}):
         """Return the metrics for anomaly detection
         by link predication.
 
         Parameters
         ----------
-        labels : dict, (defual=={"neg": 0, "pos": 1})
+        labels_map : dict, (defual=={"neg": 0, "pos": 1})
             Dictionary containing map of the labels.
 
         Returns
@@ -120,7 +146,10 @@ get_prediction
         Frame
             DataFrame or SFrame containing the aggregated results.
         """
-        probas = self.predict_class_probabilities()
-        avg_prob = self._learner.classify_by_links_probability(probas, self._test.features_ids, labels, metadata=self._test.metadata)
+        if not isinstance(features, DataSet):
+            features = self._learner.convert_data_to_format(features, labels, feature_id_col_name, metadata_col_names)
+        probas = self.predict_class_probabilities(features)
+        avg_prob = self._learner.classify_by_links_probability(probas, features.features_ids, labels_map,
+                                                               metadata=features.metadata)
         avg_prob = avg_prob.sort("mean_link_label")
         return avg_prob
