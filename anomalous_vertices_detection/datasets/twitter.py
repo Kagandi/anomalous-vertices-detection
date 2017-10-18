0fd067a4ffa81948966be099306aa8f098137966
Dima Kagan <dimakagan15@gmail.com>
2017-10-18 11:45:41 +0300

    minor fixes

--- anomalous_vertices_detection/datasets/twitter.py
+++ anomalous_vertices_detection/datasets/twitter.py
@@ -5,7 +5,7 @@ from anomalous_vertices_detection.configs.graph_config import GraphConfig
 from anomalous_vertices_detection.graphs.graph_factory import GraphFactory
 
 
-def load_data(dataset_file_name="twitter.csv.gz", labels_file_name="twitter_fake_ids.csv", labels_map=None):
+def load_data(dataset_file_name="twitter.csv.gz", labels_file_name="twitter_fake_ids.csv", labels_map=None, limit=5000000):
     data_path = os.path.join(DATA_DIR, dataset_file_name)
     if not os.path.exists(data_path):
         data_path = download_file(TWITTER_URL, dataset_file_name)
@@ -17,4 +17,4 @@ def load_data(dataset_file_name="twitter.csv.gz", labels_file_name="twitter_fake
     twitter_config = GraphConfig("twitter", data_path,
                                  is_directed=True, labels_path=labels_path,
                                  graph_type="regular", vertex_min_edge_number=10, vertex_max_edge_number=50000)
-    return GraphFactory().factory(twitter_config, labels=labels_map), twitter_config
+    return GraphFactory().factory(twitter_config, labels=labels_map, limit=5000000), twitter_config
